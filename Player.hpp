b19484a992f76f5da7c8aae29c6dfaf1b5dedfc8
Davi_Dressler <sodressler328@gmail.com>
2026-05-04 17:33:30 -0300

    Adicionando arquivos iniciais do trabalho.

---
+++ Player.hpp
@@ -0,0 +1,27 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <iostream>
+#include <string>
+
+class Player {
+
+private:
+    int id;
+    std::string name;
+    int score;
+    int timestamp;
+
+public:
+
+    Player();
+    Player(int id, std::string name, int score, int timestamp);
+    ~Player();
+
+    int getId();
+    std::string getName();
+    int getScore();
+    int getTimestamp();
+};
+
+#endif
